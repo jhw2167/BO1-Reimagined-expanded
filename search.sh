98499a13c95bc020bca0f2c03066c486aae7104c
jhw2167 <jhw2167@columbia.edu>
2021-09-23 20:55:33 -0700

    re-initial commit, staged changes for RE-ADDING MULEKICK to all maps

---
+++ search.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#searchs for string 'str' in directory 'dir' and copies valid files into new dir
+#usage "./search my/file/path myString"
+
+dir=$1
+str=$2
+dirOut='zzz_search'
+out="${dirOut}/__search.txt"
+
+
+mkdir $dirOut
+grep --exclude-dir={$dirOut,Common} -rFni $dir -e "${str}" > $out
+
+
+#copy each file where instance was found into our out dir
+while IFS='' read -r LINE || [ -n "${LINE}" ]; do
+	fp=${LINE%%:*}
+	#echo $fp | tr ./ -
+	subdir=`echo $fp | tr ./ -`
+	#echo "subdir is: ${subdir}"
+	mkdir "${dirOut}/${subdir}"
+    cp $fp "${dirOut}/${subdir}"
+done < $out
